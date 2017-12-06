2f9acbfd9d5b0940cf0b80fb57a9dcde66ce866a
jay <zenghui64@qq.com>
2017-12-06 17:32:13 +0800

    提交代码

--- snailFirstProject/App.js
+++ snailFirstProject/App.js
@@ -16,36 +16,51 @@ import {
 
 
 
-const instructions = Platform.select({
-  ios: '按下 Cmd+R 刷新,\n' +
-    'Cmd+D 或者摇一摇 弹出菜单',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+// const instructions = Platform.select({
+//   ios: '按下 Cmd+R 刷新,\n' +
+//     'Cmd+D 或者摇一摇 弹出菜单',
+//   android: 'Double tap R on your keyboard to reload,\n' +
+//     'Shake or press menu button for dev menu',
+// });
 
 export default class App extends Component<{}> {
   render() {
-    let pic = {
-      uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
-    };
+    
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
           欢迎使用 React Native!
         </Text>
-        <Text style={styles.instructions}>
-          开始编写吧, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
-        <Image source={pic} style={{width: 193, height: 110}} />
+        <Blink text = '我只是想试试看'> 
+        </Blink>
       </View>
 
     );
   }
 }
 
+
+class Blink extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { showText: true };
+
+    // 每1000毫秒对showText状态做一次取反操作
+    setInterval(() => {
+      this.setState(previousState => {
+        return { showText: !previousState.showText };
+      });
+    }, 1000);
+  }
+
+  render() {
+    // 根据当前showText的值决定是否显示text内容
+    let display = this.state.showText ? this.props.text : ' ';
+    return (
+      <Text>{display}</Text>
+    );
+  }
+}
 
 
 const styles = StyleSheet.create({
